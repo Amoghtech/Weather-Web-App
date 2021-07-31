5efeac7765162eb64526aeab059e6d79f66da1ee
Amogh Nagar <amogh.1923cs1192@kiet.edu>
2021-08-01 00:29:12 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from "./App.module.css"
 
 function App() {
   return (
-    <>
-    </>
+    <div className={styles["container"]}>
+     
+    </div>
   );
 }
 
